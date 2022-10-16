13309e4d2e05c81701319ba549c9a81f592a5b6e
abba_ye38 <abba_ye38@itstep.edu.az>
2022-10-16 17:19:15 +0400

    Add project files.

---
+++ WpfApp5/Models/Database.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp5.Models
+{
+    public class Database
+    {
+        public List<Post> Posts { get; set; }
+        public List<Subscriber> Subsribers { get; set; }
+    }
+}
---
+++ WpfApp5/Models/Subscriber.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp5.Models
+{
+    public class Subscriber
+    {
+        public List<int> PostIDs { get; set; }
+        
+    }
+}
